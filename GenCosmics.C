a3c58abea4ace690cc3c99eced9c2a04915e2586
Edward Brash <brash@jlab.org>
2016-03-14 10:17:19 +0000

    Updates

--- GenCosmics.C
+++ GenCosmics.C
@@ -50,7 +50,7 @@ Float_t         fIntRatio;
 
 // ------------------------------------------------------------------------------------------------
 
-void GenCosmics( ULong64_t nevents = 1000, 
+void GenCosmics( ULong64_t nevents = 10000, 
 		 TString fname = "data/Gen_test1.root" ) 
 {
   
