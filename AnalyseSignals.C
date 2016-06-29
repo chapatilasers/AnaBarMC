8d9e12393649fa1ec08457b5c28b4b93c48b146c
Edward Brash <brash@jlab.org>
2016-06-29 16:23:01 +0100

    Modifications to plotting macro to fit PMT histos with Landau+Gaussian

--- AnalyseSignals.C
+++ AnalyseSignals.C
@@ -1,13 +1,212 @@
+#include "TH1.h"
+#include "TF1.h"
+#include "TROOT.h"
+#include "TStyle.h"
 #include "TMath.h"
 
-Double_t fitfunction(Double_t *x, Double_t *par) {
-	Float_t xx = x[0];
-	Float_t binsize = 1000.0/100.0;
-	//Double_t f = binsize*par[0]/sqrt(2.0*3.14159265*pow(par[2],2))*exp(-0.5*(xx-par[1])*(xx-par[1])/(par[2]*par[2]));
-	Double_t f = par[3]*xx*xx+par[4]*xx+par[5]+binsize*par[0]/sqrt(2.0*3.14159265*pow(par[2],2))*exp(-0.5*(xx-par[1])*(xx-par[1])/(par[2]*par[2]));
-	return f;
+Double_t langaufun(Double_t *x, Double_t *par) {
+
+   //Fit parameters:
+   //par[0]=Width (scale) parameter of Landau density
+   //par[1]=Most Probable (MP, location) parameter of Landau density
+   //par[2]=Total area (integral -inf to inf, normalization constant)
+   //par[3]=Width (sigma) of convoluted Gaussian function
+   //
+   //In the Landau distribution (represented by the CERNLIB approximation), 
+   //the maximum is located at x=-0.22278298 with the location parameter=0.
+   //This shift is corrected within this function, so that the actual
+   //maximum is identical to the MP parameter.
+
+      // Numeric constants
+      Double_t invsq2pi = 0.3989422804014;   // (2 pi)^(-1/2)
+      Double_t mpshift  = -0.22278298;       // Landau maximum location
+
+      // Control constants
+      Double_t np = 100.0;      // number of convolution steps
+      Double_t sc =   5.0;      // convolution extends to +-sc Gaussian sigmas
+
+      // Variables
+      Double_t xx;
+      Double_t mpc;
+      Double_t fland;
+      Double_t sum = 0.0;
+      Double_t xlow,xupp;
+      Double_t step;
+      Double_t i;
+
+
+      // MP shift correction
+      mpc = par[1] - mpshift * par[0]; 
+
+      // Range of convolution integral
+      xlow = x[0] - sc * par[3];
+      xupp = x[0] + sc * par[3];
+
+      step = (xupp-xlow) / np;
+
+      // Convolution integral of Landau and Gaussian by sum
+      for(i=1.0; i<=np/2; i++) {
+         xx = xlow + (i-.5) * step;
+         fland = TMath::Landau(xx,mpc,par[0]) / par[0];
+         sum += fland * TMath::Gaus(x[0],xx,par[3]);
+
+         xx = xupp - (i-.5) * step;
+         fland = TMath::Landau(xx,mpc,par[0]) / par[0];
+         sum += fland * TMath::Gaus(x[0],xx,par[3]);
+      }
+
+      return (par[2] * step * sum * invsq2pi / par[3]);
 }
 
+TF1 *langaufit(TH1F *his, Double_t *fitrange, Double_t *startvalues, Double_t *parlimitslo, Double_t *parlimitshi, Double_t *fitparams, Double_t *fiterrors, Double_t *ChiSqr, Int_t *NDF)
+{
+   // Once again, here are the Landau * Gaussian parameters:
+   //   par[0]=Width (scale) parameter of Landau density
+   //   par[1]=Most Probable (MP, location) parameter of Landau density
+   //   par[2]=Total area (integral -inf to inf, normalization constant)
+   //   par[3]=Width (sigma) of convoluted Gaussian function
+   //
+   // Variables for langaufit call:
+   //   his             histogram to fit
+   //   fitrange[2]     lo and hi boundaries of fit range
+   //   startvalues[4]  reasonable start values for the fit
+   //   parlimitslo[4]  lower parameter limits
+   //   parlimitshi[4]  upper parameter limits
+   //   fitparams[4]    returns the final fit parameters
+   //   fiterrors[4]    returns the final fit errors
+   //   ChiSqr          returns the chi square
+   //   NDF             returns ndf
+
+   Int_t i;
+   Char_t FunName[100];
+
+   sprintf(FunName,"Fitfcn_%s",his->GetName());
+
+   TF1 *ffitold = (TF1*)gROOT->GetListOfFunctions()->FindObject(FunName);
+   if (ffitold) delete ffitold;
+
+   TF1 *ffit = new TF1(FunName,langaufun,fitrange[0],fitrange[1],4);
+   ffit->SetParameters(startvalues);
+   ffit->SetParNames("Width","MP","Area","GSigma");
+   
+   for (i=0; i<4; i++) {
+      ffit->SetParLimits(i, parlimitslo[i], parlimitshi[i]);
+   }
+
+   his->Fit(FunName,"RB0");   // fit within specified range, use ParLimits, do not plot
+
+   ffit->GetParameters(fitparams);    // obtain fit parameters
+   for (i=0; i<4; i++) {
+      fiterrors[i] = ffit->GetParError(i);     // obtain fit parameter errors
+   }
+   ChiSqr[0] = ffit->GetChisquare();  // obtain chi^2
+   NDF[0] = ffit->GetNDF();           // obtain ndf
+
+   return (ffit);              // return fit function
+
+}
+
+Int_t langaupro(Double_t *params, Double_t &maxx, Double_t &FWHM) {
+
+   // Seaches for the location (x value) at the maximum of the 
+   // Landau-Gaussian convolute and its full width at half-maximum.
+   //
+   // The search is probably not very efficient, but it's a first try.
+
+   Double_t p,x,fy,fxr,fxl;
+   Double_t step;
+   Double_t l,lold;
+   Int_t i = 0;
+   Int_t MAXCALLS = 10000;
+
+
+   // Search for maximum
+
+   p = params[1] - 0.1 * params[0];
+   step = 0.05 * params[0];
+   lold = -2.0;
+   l    = -1.0;
+
+
+   while ( (l != lold) && (i < MAXCALLS) ) {
+      i++;
+
+      lold = l;
+      x = p + step;
+      l = langaufun(&x,params);
+ 
+      if (l < lold)
+         step = -step/10;
+ 
+      p += step;
+   }
+
+   if (i == MAXCALLS)
+      return (-1);
+
+   maxx = x;
+
+   fy = l/2;
+
+
+   // Search for right x location of fy
+
+   p = maxx + params[0];
+   step = params[0];
+   lold = -2.0;
+   l    = -1e300;
+   i    = 0;
+
+
+   while ( (l != lold) && (i < MAXCALLS) ) {
+      i++;
+
+      lold = l;
+      x = p + step;
+      l = TMath::Abs(langaufun(&x,params) - fy);
+ 
+      if (l > lold)
+         step = -step/10;
+ 
+      p += step;
+   }
+
+   if (i == MAXCALLS)
+      return (-2);
+
+   fxr = x;
+
+
+   // Search for left x location of fy
+
+   p = maxx - 0.5 * params[0];
+   step = -params[0];
+   lold = -2.0;
+   l    = -1e300;
+   i    = 0;
+
+   while ( (l != lold) && (i < MAXCALLS) ) {
+      i++;
+
+      lold = l;
+      x = p + step;
+      l = TMath::Abs(langaufun(&x,params) - fy);
+ 
+      if (l > lold)
+         step = -step/10;
+ 
+      p += step;
+   }
+
+   if (i == MAXCALLS)
+      return (-3);
+
+
+   fxl = x;
+
+   FWHM = fxr - fxl;
+   return (0);
+}
 
 void AnalyseSignals(Int_t Analysis_Run_Number = 1, Int_t Analyse_Secondaries = 1, Float_t Theta_min_cut = 0.0, bool displayall = false) {
 
@@ -24,8 +223,8 @@ void AnalyseSignals(Int_t Analysis_Run_Number = 1, Int_t Analyse_Secondaries = 1
   const int MaxPMTHits = 500;
   const Float_t Finger_Edep_Max = 10.0;
   const Float_t AnaBar_Edep_Max = 5.0;
-  const Int_t Finger_NPhotons_Max = 150;
-  const Int_t AnaBar_NPhotons_Max = 50;
+  const Int_t Finger_NPhotons_Max = 1000;
+  const Int_t AnaBar_NPhotons_Max = 30;
   Float_t Prim_E, Prim_Th, Prim_Ph;
   Int_t Prim_pdg;
   Int_t Detector_Nhits;
@@ -81,8 +280,8 @@ void AnalyseSignals(Int_t Analysis_Run_Number = 1, Int_t Analyse_Secondaries = 1
   TH1F *hAnaBarPMTNphotA12 = new TH1F("AnaBarPMTNphotA12","AnaBar PMT Number of Photons A12", AnaBar_NPhotons_Max, 0, AnaBar_NPhotons_Max);
   TH1F *hAnaBarPMTNphotA13 = new TH1F("AnaBarPMTNphotA13","AnaBar PMT Number of Photons A13", AnaBar_NPhotons_Max, 0, AnaBar_NPhotons_Max);
   TH1F *hAnaBarPMTNphotA14 = new TH1F("AnaBarPMTNphotA14","AnaBar PMT Number of Photons A14", AnaBar_NPhotons_Max, 0, AnaBar_NPhotons_Max);
-  TH1F *hFingerPMTKE = new TH1F("FingerPMTKE","Finger PMT Kinetic Energy", 300, 300.0, 600.0);
-  TH1F *hAnaBarPMTKEA1 = new TH1F("AnaBarPMTKEA1","AnaBar PMT Kinetic Energy A1", 300, 300.0, 600.0);
+  TH1F *hFingerPMTKE = new TH1F("FingerPMTKE","Finger PMT Kinetic Energy", 400, 300.0, 700.0);
+  TH1F *hAnaBarPMTKEA1 = new TH1F("AnaBarPMTKEA1","AnaBar PMT Kinetic Energy A1", 400, 300.0, 700.0);
   
   TH2F *hFinger_Edep_vs_Nphot = new TH2F("FingerEdepVsNphot", "Finger Edep vs. Number of Photons", Finger_NPhotons_Max, 0, Finger_NPhotons_Max, 100, 0.01, Finger_Edep_Max);
   TH2F *hAnaBar_Edep_vs_Nphot = new TH2F("AnaBarEdepVsNphot", "AnaBar Edep vs. Number of Photons", AnaBar_NPhotons_Max, 0, AnaBar_NPhotons_Max, 100, 0.01, 5);
@@ -150,7 +349,8 @@ void AnalyseSignals(Int_t Analysis_Run_Number = 1, Int_t Analyse_Secondaries = 1
 
     bool trigger = false;
     bool finger_hit = false;
-    bool anabar_hit = false;
+    bool anabar_top_hit = false;
+    bool anabar_bottom_hit = false;
     int j_finger = 0;
     int j_anabar = 0;
     for (Int_t j=0; j < Detector_Nhits ; j++) {
@@ -159,14 +359,20 @@ void AnalyseSignals(Int_t Analysis_Run_Number = 1, Int_t Analyse_Secondaries = 1
 		j_finger = j;
 		//cout<<"hit in finger";
 	}
-	if (Detector_id[j] == 1 && !anabar_hit) {
-		anabar_hit = true;
+	if (Detector_id[j] == 1 && !anabar_top_hit) {
+		anabar_top_hit = true;
+		j_anabar = j;
+	}
+	if (Detector_id[j] == 14 && !anabar_bottom_hit) {
+		anabar_bottom_hit = true;
 		j_anabar = j;
 	}
     }
 
-    if (finger_hit && anabar_hit) trigger = true; 
+    if (finger_hit && anabar_top_hit && anabar_bottom_hit) trigger = true; 
+    //if (finger_hit && anabar_top_hit) trigger = true; 
     //if (finger_hit) trigger = true; 
+    
     if (trigger) {
         //for (Int_t j=0; j<15; j++) { std::cout << "j = " << j << " Nphotons = " << PMT_Nphotons[j] << std::endl; }
   	hAnaBarPMTNphotA1->Fill(PMT_Nphotons[0]);
@@ -378,34 +584,129 @@ void AnalyseSignals(Int_t Analysis_Run_Number = 1, Int_t Analyse_Secondaries = 1
   c8->cd(4);
   htracklength_vs_AnaBar_Edep->Draw("COLZ");
   
+  printf("Fittingi A1 ...\n");
+  // Setting fit range and start values
+  Double_t fr[2];
+  Double_t sv[4], pllo[4], plhi[4], fp[4], fpe[4];
+  pllo[0]=0.05; pllo[1]=0.5; pllo[2]=1.0; pllo[3]=0.04;
+  plhi[0]=10.0; plhi[1]=50.0; plhi[2]=10000.0; plhi[3]=5.0;
+  sv[0]=1.8; sv[1]=5.0; sv[2]=1400.0; sv[3]=3.0;
+  Double_t chisqr;
+  Int_t    ndf;
+  Double_t SNRPeak, SNRFWHM;
+  
   c9->cd(1);
   hAnaBarPMTNphotA1->Draw();
+  fr[0]=0.1*hAnaBarPMTNphotA1->GetMean();
+  fr[1]=4.0*hAnaBarPMTNphotA1->GetMean();
+  TF1 *fitsnr = langaufit(hAnaBarPMTNphotA1,fr,sv,pllo,plhi,fp,fpe,&chisqr,&ndf);
+  langaupro(fp,SNRPeak,SNRFWHM);
+  fitsnr->Draw("SAME");
+  
   c9->cd(2);
   hAnaBarPMTNphotA2->Draw();
+  fr[0]=0.1*hAnaBarPMTNphotA2->GetMean();
+  fr[1]=4.0*hAnaBarPMTNphotA2->GetMean();
+  TF1 *fitsnr = langaufit(hAnaBarPMTNphotA2,fr,sv,pllo,plhi,fp,fpe,&chisqr,&ndf);
+  langaupro(fp,SNRPeak,SNRFWHM);
+  fitsnr->Draw("SAME");
+  
   c9->cd(3);
   hAnaBarPMTNphotA3->Draw();
+  fr[0]=0.1*hAnaBarPMTNphotA3->GetMean();
+  fr[1]=4.0*hAnaBarPMTNphotA3->GetMean();
+  TF1 *fitsnr = langaufit(hAnaBarPMTNphotA3,fr,sv,pllo,plhi,fp,fpe,&chisqr,&ndf);
+  langaupro(fp,SNRPeak,SNRFWHM);
+  fitsnr->Draw("SAME");
+  
   c9->cd(4);
   hAnaBarPMTNphotA4->Draw();
+  fr[0]=0.1*hAnaBarPMTNphotA4->GetMean();
+  fr[1]=4.0*hAnaBarPMTNphotA4->GetMean();
+  TF1 *fitsnr = langaufit(hAnaBarPMTNphotA4,fr,sv,pllo,plhi,fp,fpe,&chisqr,&ndf);
+  langaupro(fp,SNRPeak,SNRFWHM);
+  fitsnr->Draw("SAME");
+  
   c9->cd(5);
   hAnaBarPMTNphotA5->Draw();
+  fr[0]=0.1*hAnaBarPMTNphotA5->GetMean();
+  fr[1]=4.0*hAnaBarPMTNphotA5->GetMean();
+  TF1 *fitsnr = langaufit(hAnaBarPMTNphotA5,fr,sv,pllo,plhi,fp,fpe,&chisqr,&ndf);
+  langaupro(fp,SNRPeak,SNRFWHM);
+  fitsnr->Draw("SAME");
+  
   c9->cd(6);
   hAnaBarPMTNphotA6->Draw();
+  fr[0]=0.1*hAnaBarPMTNphotA6->GetMean();
+  fr[1]=4.0*hAnaBarPMTNphotA6->GetMean();
+  TF1 *fitsnr = langaufit(hAnaBarPMTNphotA6,fr,sv,pllo,plhi,fp,fpe,&chisqr,&ndf);
+  langaupro(fp,SNRPeak,SNRFWHM);
+  fitsnr->Draw("SAME");
+  
   c9->cd(7);
   hAnaBarPMTNphotA7->Draw();
+  fr[0]=0.1*hAnaBarPMTNphotA7->GetMean();
+  fr[1]=4.0*hAnaBarPMTNphotA7->GetMean();
+  TF1 *fitsnr = langaufit(hAnaBarPMTNphotA7,fr,sv,pllo,plhi,fp,fpe,&chisqr,&ndf);
+  langaupro(fp,SNRPeak,SNRFWHM);
+  fitsnr->Draw("SAME");
+  
   c9->cd(8);
   hAnaBarPMTNphotA8->Draw();
+  fr[0]=0.1*hAnaBarPMTNphotA8->GetMean();
+  fr[1]=4.0*hAnaBarPMTNphotA8->GetMean();
+  TF1 *fitsnr = langaufit(hAnaBarPMTNphotA8,fr,sv,pllo,plhi,fp,fpe,&chisqr,&ndf);
+  langaupro(fp,SNRPeak,SNRFWHM);
+  fitsnr->Draw("SAME");
+  
   c9->cd(9);
   hAnaBarPMTNphotA9->Draw();
+  fr[0]=0.1*hAnaBarPMTNphotA9->GetMean();
+  fr[1]=4.0*hAnaBarPMTNphotA9->GetMean();
+  TF1 *fitsnr = langaufit(hAnaBarPMTNphotA9,fr,sv,pllo,plhi,fp,fpe,&chisqr,&ndf);
+  langaupro(fp,SNRPeak,SNRFWHM);
+  fitsnr->Draw("SAME");
+  
   c9->cd(10);
   hAnaBarPMTNphotA10->Draw();
+  fr[0]=0.1*hAnaBarPMTNphotA10->GetMean();
+  fr[1]=4.0*hAnaBarPMTNphotA10->GetMean();
+  TF1 *fitsnr = langaufit(hAnaBarPMTNphotA10,fr,sv,pllo,plhi,fp,fpe,&chisqr,&ndf);
+  langaupro(fp,SNRPeak,SNRFWHM);
+  fitsnr->Draw("SAME");
+  
   c9->cd(11);
   hAnaBarPMTNphotA11->Draw();
+  fr[0]=0.1*hAnaBarPMTNphotA11->GetMean();
+  fr[1]=4.0*hAnaBarPMTNphotA11->GetMean();
+  TF1 *fitsnr = langaufit(hAnaBarPMTNphotA11,fr,sv,pllo,plhi,fp,fpe,&chisqr,&ndf);
+  langaupro(fp,SNRPeak,SNRFWHM);
+  fitsnr->Draw("SAME");
+  
   c9->cd(12);
   hAnaBarPMTNphotA12->Draw();
+  fr[0]=0.1*hAnaBarPMTNphotA12->GetMean();
+  fr[1]=4.0*hAnaBarPMTNphotA12->GetMean();
+  TF1 *fitsnr = langaufit(hAnaBarPMTNphotA12,fr,sv,pllo,plhi,fp,fpe,&chisqr,&ndf);
+  langaupro(fp,SNRPeak,SNRFWHM);
+  fitsnr->Draw("SAME");
+  
   c9->cd(13);
   hAnaBarPMTNphotA13->Draw();
+  fr[0]=0.1*hAnaBarPMTNphotA13->GetMean();
+  fr[1]=4.0*hAnaBarPMTNphotA13->GetMean();
+  TF1 *fitsnr = langaufit(hAnaBarPMTNphotA13,fr,sv,pllo,plhi,fp,fpe,&chisqr,&ndf);
+  langaupro(fp,SNRPeak,SNRFWHM);
+  fitsnr->Draw("SAME");
+  
   c9->cd(14);
   hAnaBarPMTNphotA14->Draw();
+  fr[0]=0.1*hAnaBarPMTNphotA14->GetMean();
+  fr[1]=4.0*hAnaBarPMTNphotA14->GetMean();
+  TF1 *fitsnr = langaufit(hAnaBarPMTNphotA14,fr,sv,pllo,plhi,fp,fpe,&chisqr,&ndf);
+  langaupro(fp,SNRPeak,SNRFWHM);
+  fitsnr->Draw("SAME");
+  
 
   c10->cd(1);
   hFingerPMTKE->Draw();
@@ -434,3 +735,18 @@ void AnalyseSignals(Int_t Analysis_Run_Number = 1, Int_t Analyse_Secondaries = 1
   hAnaBarPMTNphotA1->Draw();
 
 }
+
+
+void langaus() {
+   // Fill Histogram
+   Int_t data[100] = {0,0,0,0,0,0,2,6,11,18,18,55,90,141,255,323,454,563,681,
+                    737,821,796,832,720,637,558,519,460,357,291,279,241,212,
+                    153,164,139,106,95,91,76,80,80,59,58,51,30,49,23,35,28,23,
+                    22,27,27,24,20,16,17,14,20,12,12,13,10,17,7,6,12,6,12,4,
+                    9,9,10,3,4,5,2,4,1,5,5,1,7,1,6,3,3,3,4,5,4,4,2,2,7,2,4};
+   TH1F *hSNR = new TH1F("snr","Signal-to-noise",400,0,400);
+
+   for (Int_t i=0; i<100; i++) hSNR->Fill(i,data[i]);
+
+}
+
