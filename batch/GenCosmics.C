f11a0fac25542ddb519d98fbac65cbb86ec47807
chapatilasers <nandhu92@gmail.com>
2022-06-28 14:56:47 -0400

    Updates

--- batch/GenCosmics.C
+++ batch/GenCosmics.C
@@ -65,7 +65,7 @@ void GenCosmics( int nevents = 100,
 
   // Initialise output
   TString fname;
-  fname.Form("/home/nandhu/CDetOptical/batch/data/AnaBarMC_Gen_%d.root",run_number);
+  fname.Form("~/CDetOptical/batch/data/AnaBarMC_Gen_%d.root",run_number);
   fOutFileName = fname;
   InitOutput();
 
