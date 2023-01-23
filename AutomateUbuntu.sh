ed8012e3208a8ca60fb315a6270732129a742371
Edward Brash <brash99@gmail.com>
2023-01-23 15:33:54 -0500

    updates

---
+++ AutomateUbuntu.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#Initialize values
+fPDGVal=${1:-"13"}
+start=${2:-7001}
+end=${3:-7080}
+nevents=${4:-96}
+
+#The generation code
+echo "Generating input ROOT files for given particle ..."
+cd batch
+for (( i=$start ; i<=$end ; i++ ))
+do
+root -b -q 'GenParticles.C('$fPDGVal','$nevents','$i')'
+done
+cd ..
+echo "Done"
+
+#Generate the runlist
+echo "Generating run list ..."
+echo $start > batch/runlist_test
+for (( i=$(($start + 1)) ; i<=$end ; i++ ))
+do
+echo "$i" >> batch/runlist_test
+done
+echo "Done"
+
+#Simulation
+echo "Submitting simulation jobs ..."
+cd ~/CDetOptical/batch
+./sendjobsubuntu.sh runlist_test
+echo "Done"
+
+
+
+
+
