edadb8760a92609ce712f59fd6d9352c5b1ddda7
brash99 <brash@jlab.org>
2023-03-28 21:28:53 -0400

    Updates

--- batch/AnaBarNeutronMacOS.sh
+++ batch/AnaBarNeutronMacOS.sh
@@ -11,7 +11,7 @@ echo "/control/macroPath $MACRO_PATH"	 	                         >   $MCMACRO
 echo "/AnaBarMC/physics/addPhysics standard_opt3"                        >>   $MCMACRO
 echo "/AnaBarMC/physics/optical 1"	                                 >>  $MCMACRO
 echo "/AnaBarMC/physics/hadronic 1"	                                 >>  $MCMACRO
-echo "/AnaBarMC/detector/AnaBarXpos 20.00"	                         >>  $MCMACRO
+echo "/AnaBarMC/detector/AnaBarXpos 45.00"	                         >>  $MCMACRO
 echo "/AnaBarMC/detector/AnaBarYpos 0.00"	                         >>  $MCMACRO
 echo "/AnaBarMC/detector/AnaBarZpos 150.00"	                         >>  $MCMACRO
 echo "/AnaBarMC/detector/NumberOfLayers 14"	                         >>  $MCMACRO
