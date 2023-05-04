e64037e5f9fb80e77535ab13157330e06a06c9b0
Edward Brash <brash@jlab.org>
2023-05-04 17:51:54 -0400

    Updates

--- AutomateUbuntu.sh
+++ AutomateUbuntu.sh
@@ -4,14 +4,16 @@
 fPDGVal=${1:-"13"}
 start=${2:-7001}
 end=${3:-7080}
-nevents=${4:-96}
+nevents=${4:-100}
+xvalue=${5:-0}
+zvalue=${6:-0}
 
 #The generation code
 echo "Generating input ROOT files for given particle ..."
 cd batch
 for (( i=$start ; i<=$end ; i++ ))
 do
-root -b -q 'GenParticles.C('$fPDGVal','$nevents','$i')'
+root -b -q 'GenParticles.C('$fPDGVal','$nevents','$i'i,'$xvalue','$zvalue')'
 done
 cd ..
 echo "Done"
