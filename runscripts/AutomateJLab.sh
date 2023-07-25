73227c782638c0b76b851616378646749e40b46c
Edward Brash <brash@jlab.org>
2023-07-25 13:01:59 -0400

    Updates

---
+++ runscripts/AutomateJLab.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#Initialize values
+fPDGVal=${1:-"13"}
+start=${2:-7001}
+end=${3:-7064}
+nevents=${4:-100}
+xvalue=${5:-0}
+zvalue=${6:-0}
+
+#The generation code
+echo "Generating input ROOT files for given particle ..."
+cd batch
+for (( i=$start ; i<=$end ; i++ ))
+do
+root -b -q 'GenParticles.C('$fPDGVal','$nevents','$i','$xvalue','$zvalue')'
+done
+cd ..
+echo "Done"
+
+#Generate the runlist
+echo "Generating run list ..."
+echo $start > batch/runlist_test
+for (( i=$(($start + 1)) ; i<=$end ; i++ ))
+do
+echo "$i" >> batch/runlist_test
+done
+echo "Done"
+
+#Simulation
+echo "Submitting simulation jobs ..."
+cd ~/CDetMC/batch
+./sendjobsjlab.sh runlist_test
+echo "Done"
